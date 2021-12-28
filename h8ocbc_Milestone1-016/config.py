1ebb8dbbb6fcde8f312b0aaa777dd58ce26e974a
Esra <esradelimamanurung1999@gmail.com>
2021-12-28 12:58:37 +0700

    final project python

---
+++ h8ocbc_Milestone1-016/config.py
@@ -0,0 +1,23 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gmmteediksaiwr:6d30d75f392f4e048332bdaf7c3bcbbb0628d60573ca2407c1ce00ba313d1f34@ec2-52-204-72-14.compute-1.amazonaws.com:5432/d9h1drs38pkrm1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
